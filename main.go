c8296e6dbf58add80a22f4952df889ae44c7469b
darcinc <phoehne@mac.com>
2021-10-14 09:16:07 -0400

    Cleaning up and working on handling injected types vs injected structs.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"reflect"
 )
 
@@ -36,9 +37,9 @@ func (dm DependencyManager) setField(item reflect.Value, field reflect.StructFie
 	var v interface{}
 	var found bool
 	if ok {
-		v, found = dm.GetInstance(injectFrom)
+		v, found = dm.Get(injectFrom)
 	} else {
-		v, found = dm.GetInstance(field.Name)
+		v, found = dm.Get(field.Name)
 	}
 	if !found {
 		dm.setByName(item, field.Name, reflect.Zero(field.Type))
@@ -47,39 +48,62 @@ func (dm DependencyManager) setField(item reflect.Value, field reflect.StructFie
 	}
 }
 
-func (dm DependencyManager) GetInstance(name string) (interface{}, bool) {
-	anInstance, ok := dm.guts[name]
-	if !ok {
-		if someType, ok := dm.types[name]; !ok {
-			return nil, false
-		} else {
-			result := reflect.New(someType)
-			for i := 0; i < someType.NumField(); i++ {
-				dm.setField(result, someType.Field(i))
-			}
-			return result.Interface(), true
-		}
+func (dm DependencyManager) constructFromType(aType reflect.Type) (interface{}, bool) {
+	result := reflect.New(aType)
+	for i := 0; i < aType.NumField(); i++ {
+		dm.setField(result, aType.Field(i))
 	}
+	return result.Elem().Interface(), true
+}
 
+func (dm DependencyManager) returnValue(anInstance interface{}) (interface{}, bool) {
 	t, v := reflect.TypeOf(anInstance), reflect.ValueOf(anInstance)
 
 	if t.Kind() == reflect.Func {
 		results := v.Call([]reflect.Value{reflect.ValueOf(dm)})
-		return results[0].Interface(), ok
+		return results[0].Interface(), results[1].Bool()
 	} else if t.Kind() == reflect.Struct {
-		// Todo: If the inject tag is present, and the value is zero, inject it.
-		return anInstance, ok
+		return anInstance, true
 	} else {
-		return anInstance, ok
+		return anInstance, true
 	}
 }
 
-type Person struct {
-	FirstName, LastName string
+func (dm DependencyManager) Get(name string) (interface{}, bool) {
+	anInstance, ok := dm.guts[name]
+	if aType, hasType := dm.types[name]; !ok && hasType {
+		return dm.constructFromType(aType)
+	}
+
+	return dm.returnValue(anInstance)
 }
 
-func (p Person) String() string {
-	return fmt.Sprintf("%s, %s", p.LastName, p.FirstName)
+func (dm DependencyManager) MustGet(name string) interface{} {
+	someAsset, ok := dm.Get(name)
+	if !ok {
+		panic(fmt.Sprintf("failed to find a registered value for %s", name))
+	}
+
+	return someAsset
+}
+
+func (dm DependencyManager) Inject(someType reflect.Type) interface{} {
+	result, _ := dm.constructFromType(someType)
+	return result
+}
+
+type StorageService struct {
+	Host    string
+	Port    int
+	Account string `inject:"accountId"`
+}
+
+type StorageHandler struct {
+	Service StorageService `inject:"storageConn"`
+}
+
+func (sh StorageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func main() {
@@ -88,45 +112,39 @@ func main() {
 		types: map[string]reflect.Type{},
 	}
 
-	t := reflect.TypeOf(Person{})
-
-	//dm.Register("FirstName", "Joe")
-	//dm.Register("LastName", "User")
-	dm.Register("Person", t)
-	dm.Register("Ed", Person{
-		FirstName: "Ed",
-		LastName:  "Smith",
-	})
-
-	func() {
-		type Froopie struct {
-			Age int
+	dm.Register("storageEndpoint", StorageService{Host: "foo.bar.com", Port: 1234})
+	dm.Register("accountId", "ABC123")
+	dm.Register("storageConn", func(dm DependencyManager) (interface{}, bool) {
+		var endpoint StorageService
+		var ok bool
+		if endpoint, ok = dm.MustGet("storageEndpoint").(StorageService); !ok {
+			return nil, false
 		}
 
-		dm.Register("Froopie", reflect.ValueOf(Froopie{}))
-	}()
+		var accountId string
+		if accountId, ok = dm.MustGet("accountId").(string); !ok {
+			return nil, false
+		}
 
-	val, found := dm.GetInstance("Person")
-	if !found {
-		fmt.Println("Did not find the person")
+		endpoint.Account = accountId
+		return endpoint, ok
+	})
+	dm.Register("storageHandler", reflect.TypeOf(StorageHandler{}))
+
+	se, _ := dm.Get("storageConn")
+	if endpoint, ok := se.(StorageService); ok {
+		fmt.Printf("Connecting to %s:%d with %s\n", endpoint.Host, endpoint.Port, endpoint.Account)
 	}
-	fmt.Printf("%v\n", val)
 
-	val, found = dm.GetInstance("Ed")
-	if !found {
-		fmt.Println("Ed went missing")
-	}
-
-	if ed, ok := val.(Person); !ok {
-		fmt.Println("What we got back wasn't Ed")
+	if sh, ok := dm.MustGet("storageHandler").(StorageHandler); !ok {
+		fmt.Printf("Failed to set up application - exiting")
 	} else {
-		fmt.Printf("%v\n", ed)
+		fmt.Printf("Connecting to %s:%d with %s\n", sh.Service.Host, sh.Service.Port, sh.Service.Account)
 	}
 
-	f, ok := dm.GetInstance("Froopie")
-	if !ok {
-		fmt.Println("No froopie")
+	if sh, ok := dm.Inject(reflect.TypeOf(StorageHandler{})).(StorageHandler); ok {
+		fmt.Printf("Connecting to %s:%d with %s\n", sh.Service.Host, sh.Service.Port, sh.Service.Account)
 	} else {
-		fmt.Println("got %v\n", f)
+		fmt.Printf("Failed to set up application - exiting")
 	}
 }
